a2cc78d6c7d60ab2a3d1fdc0e73bcf61c61cea37
proxcasy <proxcasy@gmail.com>
2018-05-22 22:33:47 +0300

    Async socket
    Am ales sa folosesc socket asincron deoarece, in urma documentarii in mediul online am ajuns la concluzia ca este mai usor de folosit si mai eficient. Acest material este o forma primara a aplicatiei client-server care v-a realiza comunicarea remote intre interfata si automat.

---
+++ Sincretic2APP/Client/ClientState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+
+namespace Client
+{
+    class ClientState
+    {// State object for receiving data from remote device.
+        // Client socket.
+        public Socket workSocket = null;
+        // Size of receive buffer.
+        public const int BufferSize = 256;
+        // Receive buffer.
+        public byte[] buffer = new byte[BufferSize];
+        // Received data string.
+        public StringBuilder sb = new StringBuilder();
+    }
+}
